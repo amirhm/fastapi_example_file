795270e625834c34a872300d4fa2f0056cd7973c
amirhm <amirhm@gmail.com>
2020-09-22 23:24:41 +0200

    format corrected

--- main.py
+++ main.py
@@ -2,30 +2,38 @@ import fastapi
 from pydantic import BaseModel
 from fastapi import File, UploadFile
 from fastapi.responses import StreamingResponse
+from fastapi.responses import FileResponse
+
 
 d = []
 
+
 class cmd(BaseModel):
-    cmd:str = 'test'
-    param:str = 'f'
+    cmd: str = "test"
+    param: str = "f"
+
 
 app = fastapi.FastAPI()
-files = {'file': open('client.py', 'rb')}
+files = {"file": open("client.py", "rb")}
 
-@app.get('/')
+
+@app.get("/")
 def main():
-    return {'answer':'OK'}
+    return {"answer": "OK"}
 
-@app.get('/rcv')
-def send_data(idx:int):
-    
-    return {'first entry':d[idx].cmd,'second entry':d[idx].param}
 
-@app.post('/cmd')
-def receive_cmd(f:cmd):
+@app.get("/rcv")
+def send_data(idx: int):
+
+    return {"first entry": d[idx].cmd, "second entry": d[idx].param}
+
+
+@app.post("/cmd")
+def receive_cmd(f: cmd):
     d.append(f)
     breakpoint()
-    return {'first entry':f.cmd,'second entry':f.param}
+    return {"first entry": f.cmd, "second entry": f.param}
+
 
 @app.post("/files/")
 async def create_file(file: bytes = File(...)):
@@ -38,15 +46,16 @@ async def create_upload_file(file: UploadFile = File(...)):
     print(contents)
     return {"filename": file.filename}
 
+
 @app.get("/getfile/")
 async def send_file():
-    print('test')
-    file_like = open('Untitled.mp4','rb')
+    print("test")
+    file_like = open("Untitled.mp4", "rb")
     return StreamingResponse(file_like, media_type="video/mp4")
 
-from fastapi.responses import FileResponse
+
 @app.get("/getfile2/")
 async def get_file():
-    print('test')
-#    file_like = open('Untitled.mp4','rb')
-    return FileResponse('Untitled.mp4')
+    print("test")
+    #    file_like = open('Untitled.mp4','rb')
+    return FileResponse("Untitled.mp4")
